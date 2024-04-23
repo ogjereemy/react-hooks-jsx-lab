817859501c5860bec4bb0e0550288ccabdf66627
ogola <ogolajeremy601@gmail.com>
2024-04-23 14:47:20 +0300

    adds react jsx lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+ const colorStyle = {
+    color : "firebrick"
+ }
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+  <div id="home">Home
+      <h1 style= {colorStyle} >
+        {name} is a Web Developer from {city}
+      </h1>
+  </div>
+  )
 }
 
 export default Home;
